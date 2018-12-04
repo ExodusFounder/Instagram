9900ff847cfac231e18fc93a32c8b2a83c515890
ExodusFounder <45518926+ExodusFounder@users.noreply.github.com>
2018-12-03 23:11:06 -0500

    Update example.py

--- example.py
+++ example.py
@@ -10,10 +10,10 @@ from src.follow_protocol import follow_protocol
 from src.unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
-    login="username",
-    password="password",
+    login="evilproductions",
+    password="Erikvjr123@#",
     like_per_day=1000,
-    comments_per_day=0,
+    comments_per_day=25,
     tag_list=['follow4follow', 'f4f', 'cute', 'l:212999109'],
     tag_blacklist=['rain', 'thunderstorm'],
     user_blacklist={},
